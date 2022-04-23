22abb107ec46db3881c8fcc173fa77e6975a4177
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-04-23 07:38:43 +0100

    Navbar Component Updated

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import Home from "./Routes/Home"
 import About from "./Routes/About"
 import Product from "./Routes/Product"
 import Contact from "./Routes/Contact"
+import Navbar from "./Components/Navbar/Navbar"
+import Footer from "./Components/Footer/Footer"
+import "./Components/Fontawesome"
 
 function App() {
   return (
     <Router>
+      <div className='App'>
+        <Navbar />
+      </div>
       <Routes>        
         <Route exact path='/' element={<Home />}></Route>
         <Route exact path='/about' element={<About />}></Route>
         <Route exact path='/product' element={<Product />}></Route>        
         <Route exact path='/contact' element={<Contact />}></Route>
       </Routes>
+      <div>
+        <Footer />
+      </div>
     </Router>
   );
 }
 
-export default App;
+export default App; 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "../Navbar/Navbar.scss"
+import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome'
+import { useState, useEffect } from 'react'
+// import logo from "../../Assets/logo.png"
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 768) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    useEffect(() => {
+        showButton()
+    }, []);
+
+    window.addEventListener('resize', showButton)
+
+    return (
+        <div className='bg-light'>
+            <nav className='navbarT container-fluid py-3'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}><img src="" alt="logo.png" /></Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <FA icon={click ? 'times': 'bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to="/" className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/about" className='nav-links' onClick={closeMobileMenu}>About</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/product" className='nav-links' onClick={closeMobileMenu}>Products</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/contact" className='nav-links' onClick={closeMobileMenu}>Contact Us</Link>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+
+        </div>
+    )
 }
 
 export default Navbar
