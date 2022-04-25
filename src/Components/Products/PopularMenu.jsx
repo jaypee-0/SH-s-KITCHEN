30bb367f68a68e1dc5c3b66c4b67a5d4bf3d1665
Adeyemi Adekunle <bamidek@gmail.com>
2022-04-25 11:42:39 +0100

    yemi

--- src/Components/Products/PopularMenu.jsx
+++ src/Components/Products/PopularMenu.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { popularMenuFood } from './products-data';
-import Product_Card from './Product_Card';
 
 
 
-const PopularMenu = () =>  {
+const PopularMenu = () => {
     return (
         <section>
             <Container>
@@ -15,10 +14,21 @@ const PopularMenu = () =>  {
                         <h2>Popular Food Menu</h2>
                     </Col>
                     {popularMenuFood.map((item) => (
-                            <Col lg='3' key={item.id}>
-                              <Product_Card item={item} />
-                            </Col>
-                        ))}
+                        <Col lg='3' key={item.id}>
+                            <div className="single_product">
+                                <div className="product_img">
+                                    <img src={item.imgUrl} alt="" className="w100" />
+                                </div>
+                                <div className="product_content">
+                                    <h6>{item.title}</h6>
+                                    <div className="d-flex aligh-items-center justify-content-between">
+                                        <span className="price"> Price: $<span>{item.price}</span></span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </Col>
+                    ))}
                 </Row>
             </Container>
         </section>
