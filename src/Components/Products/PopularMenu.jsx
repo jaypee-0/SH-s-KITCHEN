63bee9dbb29133c46387b09bbfa2dab3a0f65e1b
Adeyemi Adekunle <bamidek@gmail.com>
2022-04-25 11:57:17 +0100

    latest

--- src/Components/Products/PopularMenu.jsx
+++ src/Components/Products/PopularMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { popularMenuFood } from './products-data';
+import './product.css'
 
 
 
