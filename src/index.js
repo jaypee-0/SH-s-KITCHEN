a8f7b39f1b886057d07cf31040396d1edfd788ed
MargaretKerubo <98818039+MargaretKerubo@users.noreply.github.com>
2022-04-26 01:13:40 +0300

    Latest on contact page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 <<<<<<< HEAD
 =======
-import 'remixicon/fonts/remixicon.css'
+import 'remixicon/fonts/remixicon.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 >>>>>>> 9d062bb6bdfcfcd6139901bd72f1a0ff105fa089
