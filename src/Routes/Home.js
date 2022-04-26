26dfcab615132314b4076358c30b8bdcc46ad77a
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-04-26 03:24:08 +0100

    resolved merge conflict on popularMenu.jsx

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,14 +1,11 @@
 import React from 'react'
-
 import PopularMenu from '../Components/Products/PopularMenu'
 
 const Home = () => {
   return ( 
     <div>
-<PopularMenu />
-
-    </div>
-    
+      <PopularMenu />
+    </div>    
   )
 }
 
