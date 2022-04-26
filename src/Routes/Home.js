a8b901c10167e4b14fe764033ba92b91991a1afb
Adeyemi Adekunle <bamidek@gmail.com>
2022-04-26 10:19:18 +0100

    latest

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,24 +1,17 @@
 import React from 'react'
-<<<<<<< HEAD
-=======
 import PopularMenu from '../Components/Products/PopularMenu'
 import HeroSection from '../Components/Home/HeroSection'
 import HomeServices from '../Components/Home/HomeServices'
->>>>>>> 779d3e355630d56cbb854588aa12dd2b6ff980a9
+import Testimonial from '../Components/Testimonial/Testimonial'
 
 const Home = () => {
   return ( 
     <div>
-<<<<<<< HEAD
-
-    </div>
-    
-=======
       <HeroSection />
       <HomeServices />
       <PopularMenu />
+      <Testimonial />
     </div>    
->>>>>>> 779d3e355630d56cbb854588aa12dd2b6ff980a9
   )
 }
 
