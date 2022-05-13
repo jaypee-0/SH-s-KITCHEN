4c3cef12e108aea8d4d5eb28433c12185c1e619b
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-05-13 11:01:01 +0100

    reduced height of 404

--- src/Components/404/Error404.jsx
+++ src/Components/404/Error404.jsx
@@ -5,7 +5,7 @@ import spilledCoffee from './image/spilledCoffee.png'
 
 const Error404 = () => {
   return (
-    <div id='error' className='vh-100 justify-content-center align-items-center container d-flex flex-column'>
+    <div id='error' className='vh-75 justify-content-center align-items-center container d-flex flex-column'>
         <img className='' src={spilledCoffee} alt="" />
         <h2 className='my-3'>404 ERROR</h2>
         <h5 className='mb-5 text-center'>Whatever you're looking for is not here</h5>
